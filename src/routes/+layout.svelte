e0c468d04dcdd4b4b7a6f5adb32456a8cc27bd96
Tom Weston <tomowest23@icloud.com>
2025-10-15 14:33:24 +0800

    Added an explaination modal which explains how to troubleshoot render spindown issues with backend

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,33 +3,75 @@
 	import logo from '$lib/assets/TOMMY.png';
 	import { signOut } from '$lib/auth/auth-methods';
 	import { slide } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
+	import alert from '$lib/assets/alert.svg';
 
 	let { data, children } = $props();
+	let help = $state(false);
 	let username = $derived(data.user?.name ?? null);
 
+	const togglehelp = () => {
+		help = !help;
+	};
 </script>
 
 <svelte:head>
 	<title>Tommy's To-Dos</title>
 </svelte:head>
 
-<header>
-	<div>
+<header class="page-header">
+	<div class="logo-container">
 		<img src={logo} class="logo" alt="to-do Logo" />
 	</div>
-	<div>
+	<div class="title-container">
 		{#if username}
 			<h1 class="page-title" transition:slide={{ duration: 300 }}>{username}'s Tasks</h1>
 		{:else}
 			<h1 class="page-title" transition:slide={{ duration: 300 }}>Tommy's To-Dos</h1>
 		{/if}
 	</div>
-	<div>
+	<div class="button-container">
 		{#if data.user}
+			<button class="help-button" onclick={togglehelp}>Not Working?</button>
 			<button class="logout-button" onclick={async () => await signOut()}>Log Out</button>
 		{/if}
 	</div>
 </header>
+
+{#if help}
+	<!-- Help Popup -->
+	<div class="help-backdrop"></div>
+	<div class="help-popup-container" transition:fade={{ duration: 200 }}>
+		<header class="help-header">
+			<img src={alert} alt="Alert" class="alert" />
+			<h1 class="help-title">Fix Guide <br /></h1>
+			<button class="close-help" onclick={togglehelp}>X</button>
+		</header>
+		<div class="help-content">
+			<h1>If you're experiencing issues with accessing or creating tasks, this could be why :</h1>
+			<br />
+			<h2>
+				As this website is hosted for free, the server may be temporarily down if you are the first
+				person to access it in a while.
+			</h2>
+			<br />
+			<h2>To fix this, please try the following steps:</h2>
+			<ol>
+				<li>
+					Open this link in another tab and wait ~1 min for the server to wake up and load:<a
+						href="https://tommys-to-dos-backend.onrender.com"
+						target="_blank">https://tommys-to-dos-backend.onrender.com</a
+					>
+				</li>
+				<li>Come back to this tab and refresh the page.</li>
+				<li>
+					If the problem persists after several attempts, the best option would be to restart your
+					browser.
+				</li>
+			</ol>
+		</div>
+	</div>
+{/if}
 
 <main>
 	<!-- Main content area which is populated in page.svelte -->
@@ -42,7 +84,7 @@
 </main>
 
 <style>
-	header {
+	.page-header {
 		display: grid;
 		grid-template-columns: 1fr 3fr 1fr;
 		width: 100%;
@@ -54,24 +96,26 @@
 		align-items: center;
 	}
 
-	header div {
+	.logo-container,
+	.button-container {
 		display: flex;
 		align-items: center;
 	}
 
-	header div:nth-child(1) {
+	.logo-container {
 		justify-content: start;
 		padding-left: 1rem;
 	}
 
-	header div:nth-child(2) {
+	.title-container {
 		display: grid;
 		place-items: center;
 	}
 
-	header div:nth-child(3) {
+	.button-container {
 		justify-content: end;
 		padding-right: 2rem;
+		gap: 1rem;
 	}
 
 	.logo {
@@ -88,6 +132,7 @@
 		text-decoration: underline 5px var(--accent);
 		text-underline-offset: 0.5rem;
 		padding: 1rem;
+		overflow-x: hidden;
 	}
 
 	main {
@@ -124,4 +169,110 @@
 		box-shadow: var(--foreground) 0px 0px;
 		transform: translate(2px, 2px);
 	}
+
+	.help-popup-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+		border: 2px solid var(--foreground);
+		padding: 1rem;
+		background-color: var(--contrast);
+		box-shadow: var(--foreground) 4px 4px;
+		height: 3rem;
+		width: auto;
+		z-index: 20;
+		position: fixed;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		height: fit-content;
+	}
+
+	.alert {
+		width: 3rem;
+		height: 3rem;
+	}
+
+	.help-backdrop {
+		position: fixed;
+		inset: 0;
+		background: rgba(0, 0, 0, 0.5);
+		z-index: 15;
+		height: 100%;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.help-header {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		margin-bottom: 0;
+		font-family: '8bit';
+		border-bottom: 2px dashed var(--foreground);
+		line-height: 1.5;
+		gap: 4rem;
+		padding: 0.5rem;
+	}
+	.help-title {
+		font-family: '8bit';
+		color: var(--foreground);
+		font-size: 2.5rem;
+		margin: 0;
+	}
+	.close-help {
+		cursor: pointer;
+		font-family: '8bit';
+		font-weight: bold;
+		font-size: 2rem;
+		background-color: transparent;
+		border: none;
+		color: var(--accent);
+	}
+	.help-content,
+	h1,
+	h2,
+	ol,
+	li,
+	a {
+		padding: 1rem;
+		font-size: 1rem;
+		font-family: 'Body';
+		color: var(--foreground);
+		text-align: left;
+	}
+
+	.help-content h1 {
+		font-size: 1.7rem;
+		font-weight: bold;
+	}
+
+	.help-button {
+		align-self: center;
+		margin-left: 1rem;
+		padding: 0.7rem 0.3rem;
+		font-size: 1rem;
+		background-color: var(--cream);
+		color: var(--foreground);
+		border: 2px solid var(--foreground);
+		cursor: pointer;
+		box-shadow: 3px 3px var(--foreground);
+		font-family: '8bit';
+		font-size: small;
+	}
+
+	.help-button:hover {
+		color: var(--accent);
+	}
+
+	.help-button:active {
+		box-shadow: var(--foreground) 0px 0px;
+		transform: translate(2px, 2px);
+	}
+	
 </style>
